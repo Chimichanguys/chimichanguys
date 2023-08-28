9d324d9d74a4ca016fa10675fb154eff004a5a81
SangHano <sangchulhan.mike@gmail.com>
2023-08-28 11:26:36 -0400

    edited files

--- client/chimichanguys client/src/App.jsx
+++ client/chimichanguys client/src/App.jsx
@@ -1,63 +1,17 @@
 import { useState } from 'react'
 import {Routes, Route} from "react-router-dom"
-import Register from './components/Register'
+//import Register from './components/Register'
 import Login from './components/login';
+import Dashboard from './components/dashboard';
+import './App.css';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [error, setError] = useState(null);
-
-  const signInAPI = async (username, password) => {
-    const response = await fetch('http://localhost:8000/auth/signIn', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    });
-
-    const data = await response.json();
-  
-
-
-    if (!response.ok) {
-      throw new Error(data.message || 'Failed to login');
-    }
-
-    return data;
-  };
-
-  const handleLogin = async (username, password) => {
-    try {
-      const data = await signInAPI(username, password);
-      if (data.success) { // assuming the response has a `success` field
-        setIsLoggedIn(true);
-        setError(null); // Clear any previous errors
-      }
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const renderRoutes = () => {
-    if (isLoggedIn) {
-        return (
-            <Routes>
-                <Route path="/dashboard/*" element={<Dashboard />} />
-            </Routes>
-        );
-    } else {
-        return (
-            <Routes>
-                <Route path="/register" element={<Register />}></Route>
-                <Route path="/*" element={<Login handleLogin={handleLogin} error={error} />} />
-            </Routes>
-        );
-    }
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard/*" element={<Dashboard />} />
+    </Routes>
+  );
 };
-
-return renderRoutes();
-};
-
 
 export default App;
--- client/chimichanguys client/src/components/login.jsx
+++ client/chimichanguys client/src/components/login.jsx
@@ -1,38 +1,62 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Login =(props)=>{
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-    const goToSignUp = () => {
-        navigate('/signup');
-    };
-    const submitHandler = (e) => {
-      e.preventDefault(); 
-      props.handleLogin(username, password);
-    };
-  
-    return (
-      <div>
-        <h1>Login</h1>
-        {props.error && <div>{props.error}</div>}
-        <form onSubmit={submitHandler}>
-          <div>
-            <label>Username:</label>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
-        <button onClick={goToSignUp}>Sign Up</button>
-      </div>
-    );
-  }
+  const signInAPI = async (username, password) => {
+    const response = await fetch('http://localhost:8000/auth/signIn', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || 'Failed to login');
+    }
+
+    return data;
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault(); 
+
+    try {
+      const data = await signInAPI(username, password);
+      if (data.success) { 
+        navigate('/dashboard'); 
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {error && <div>{error}</div>}
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 export default Login;
