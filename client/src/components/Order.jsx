d726a10be5616eff795787b4e157244e36f3f090
elior valenzuela <eliorvalenzuela@gmail.com>
2023-08-28 16:26:42 -0400

    orders

---
+++ client/src/components/Order.jsx
@@ -0,0 +1,18 @@
+const Order = () => {
+    return(
+        <>
+        <h1>Order</h1>
+        <ul>
+            <li>Steak</li>
+            <li>Chicken</li>
+            <li>Pico de Gallo</li>
+            <li>Guacamole</li>
+            <li>cheese</li>
+            <li>rice</li>
+            <li>beans</li>
+        </ul>
+        </>
+    )
+}
+
+export default Order
