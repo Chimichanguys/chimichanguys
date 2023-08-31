aa51ff123ba8e6452d7ce85819bff15d49ca1381
Thomas.Baumeister <thomasbaumeister3@gmail.com>
2023-08-31 15:54:00 -0500

    working admin route and add ingredient route

--- server/api/idRequired.js
+++ server/api/idRequired.js
@@ -8,7 +8,25 @@ const requireUser  = (req, res, next) =>{
       res.status(401).send({message: "User unauthorized"});
     }
   }
+
+  const requireAdmin = (req, res, next) => {
+    if (req.admin) {
+      next();
+    }else {
+      res.status(401).send({message: "User unauthorized"})
+    }
+  }
+
+  const requireUserOrAdmin = (req, res, next) => {
+    if (req.userId === Number(req.param.id) || req.admin) {
+      next();
+    } else {
+      res.status(401).send({ message: "User unauthorized" });
+    }
+  };
   
   module.exports = {
-    requireUser
+    requireUser,
+    requireAdmin,
+    requireUserOrAdmin
   }
--- server/index.js
+++ server/index.js
@@ -18,11 +18,13 @@ app.use((req, res, next) => {
     const auth = req.headers.authorization;
     const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
     try{
-      const { id } = jwt.verify(token, process.env.JWT);
-      req.userId = id;
+      const decodedToken = jwt.verify(token, process.env.JWT);
+      req.userId = decodedToken.id;
+      req.admin = decodedToken.admin || false;
     }
     catch{
       req.userId = null;
+      req.admin = false;
     }
   
     next();
